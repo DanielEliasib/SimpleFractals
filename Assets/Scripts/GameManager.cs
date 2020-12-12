0e03ed7f373aaaf06bdc5d3b4c74394518087422
DanielEliasib <daniel.eliasiv@tagwizz.com.mx>
2020-12-11 22:30:34 -0600

    Initial commit: - Fractal processor added, it saves a set of points, the system of functions and applies them for n iterations. - First simple example in game manager.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.Mathematics;
+
+public class GameManager : MonoBehaviour
+{
+    private FractalProcessor _Processor;
+    private List<float2> _PlottingData;
+    [SerializeField] float _PointSize = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<float2> A = new List<float2>();
+
+        A.Add(float2.zero);
+        A.Add(new float2(0.0f, 0.5f));
+        A.Add(new float2(0.5f, 0.5f));
+        A.Add(new float2(0.5f, 0.0f));
+
+        _Processor = new FractalProcessor(A);
+
+        //_Processor.SetFunctions(new List<System.Func<float2, float2>>()
+        //{
+        //    (float2 p) => 0.5f*p,
+        //    (float2 p) => 0.5f*p + new float2(0.5f, 0.0f),
+        //    (float2 p) => 0.5f*p + new float2(0.25f, 0.5f),
+        //});
+
+        _Processor.SetFunctions(new List<System.Func<float2, float2>>()
+        {
+            (float2 p) => 0.5f*p,
+            (float2 p) => 0.5f*p + new float2(0.5f, 0.0f),
+            (float2 p) => 0.5f*p + new float2(0.25f, 0.5f),
+        });
+
+        _Processor.CalculateForIterations(7);
+
+        List<float2> B = new List<float2>();
+        _Processor.GetData(ref B);
+
+        _PlottingData = B;
+
+        Debug.Log($"Number of points: {_PlottingData.Count}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_PlottingData != null)
+        {
+            foreach(float2 p in _PlottingData)
+            {
+                Gizmos.DrawWireSphere(new Vector3(p.x, p.y, 0.0f), _PointSize);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Processor/FractalProcessor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using Unity.Mathematics;
+
+public class FractalProcessor
+{
+    private List<float2> _A;
+    private List<Func<float2, float2>> _IFS;
+    private bool _Init;
+
+    public FractalProcessor(List<float2> initialSet)
+    {
+        if (initialSet.Count > 0)
+            _A = initialSet;
+        else
+            throw new Exception("The initial set should have at least one point.");
+
+        _Init = false;
+    }
+
+    public void SetFunctions(List<Func<float2, float2>> functions)
+    {
+        if(functions.Count > 0)
+        {
+            _IFS = functions;
+            _Init = true;
+        }
+        else
+            throw new Exception("There should be at least one function.");
+    }
+
+    public void CalculateForIterations(int n, bool randomSampling = false)
+    {
+        if (_Init)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                ApplySystem(randomSampling);
+            }
+        }
+    }
+
+    public void GetData(ref List<float2> data)
+    {
+        //! This could cause problems but this array will be too big and will probabli be problematic to copy it
+        data = _A;
+    }
+
+    private void ApplySystem(bool random = false)
+    {
+        List<float2> T = new List<float2>();
+        if (!random)
+        {
+            foreach(Func<float2, float2> f in _IFS)
+            {
+                for(int i = 0; i < _A.Count; i++)
+                {
+                    T.Add(f(_A[i]));
+                }
+            }
+        }
+        else
+        {
+            throw new NotImplementedException("Random Sampling is not implemented yet");
+        }
+
+        _A = T;
+    }
+}
